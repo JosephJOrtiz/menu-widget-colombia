7ffb173b9375829771dfa97c3fc431e6bb79a3e9
Json Jimenez <jeison@jeison.com>
2023-08-31 18:27:31 -0500

    add token access

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://adminprocolombia-dev.nivelics.co/api',
   url: 'https://adminprocolombia-dev.nivelics.co',
-  token: '4d9f50ca71c3c5992438f9750d9835c1b32eb750b7f03c1ff6084e04c606b6f0301fd15a05c6dd17fdc9c261a87d9b6bc8dd7f29f0317f1dafbaa736918156f1aee6d3d4cf808cc4a2dd90474c05fd4867c39c07e411ba5c29ea26ae7a0877531ce6c788715a327806e8cbfe9d3f4858c3c0b37e592205427efb1b39ef1921f5'
+  token: '7ca3fe47d99e00367dac1111fd63fa9b9c80f3a6358a31659f49ae2d57bcf343e360b90baa70adf235bffef90d958b42f8e53bff9690f2c6ff7160b85ed606fc471a3f5801d4131443c030a8a75d2580afdf7711bb88c93c06124c5a676231fbe5d5e7e6292a83fc1d5ff1eb841093c520b0c923f178c9270ebab84c86b0271d'
 };
 
 /*
